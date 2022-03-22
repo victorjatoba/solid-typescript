b5bef941fe7216a36fee0ac895975b7d2371b522
Victor Jatobá <victorjatoba10@gmail.com>
2022-03-22 08:51:56 -0300

    ignore .js files

---
+++ src/dependency_inversion/di_implementation.ts
@@ -0,0 +1,40 @@
+/**
+ * Dependency Inversion Principle Explained - SOLID Design Principles
+ * By Web Dev Simplified
+ *
+ * @see https://youtu.be/9oHY5TllWaU
+ */
+
+export class Store {
+  sripe: Stripe;
+  constructor(user: string) {
+    this.sripe = new Stripe(user);
+  }
+
+  purchaseBike(quantity) {
+    this.sripe.makePayment(200 * quantity * 100);
+  }
+
+  purchaseHelmet(quantity) {
+    this.sripe.makePayment(15 * quantity * 100);
+  }
+}
+
+class Stripe {
+  user: string;
+  constructor(user) {
+    this.user = user;
+  }
+
+  makePayment(amountInCents) {
+    console.log(
+      `${this.user} made a payment of ${amountInCents / 100} with Stripe`
+    );
+  }
+}
+
+class Paypal {}
+
+const store = new Store("John");
+store.purchaseBike(2);
+store.purchaseHelmet(2);
